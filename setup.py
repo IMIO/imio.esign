7e2aae82511db1ab1099a615ef285adcea4a8c1b
Gauthier Bastien <gauthier.bastien@imio.be>
2026-03-26 16:11:47 +0100

    Back to development: 1.0b4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="imio.esign",
-    version="1.0b3",
+    version="1.0b4.dev0",
     description="Integration of IMIO esign webservice",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/

98aa66f2fae74399c8bb753e355cf160d99c3855
Gauthier Bastien <gauthier.bastien@imio.be>
2026-06-02 10:23:19 +0200

    Moved on_annex_will_be_removed from testing.zcml to configure.zcml so it is activated by default

--- src/imio/esign/events.py
+++ src/imio/esign/events.py
@@ -2,6 +2,7 @@
 
 from imio.esign.utils import get_file_info
 from imio.esign.utils import get_sessions_for
+from imio.esign.utils import remove_files_from_session
 from imio.helpers.transmogrifier import get_correct_id
 from os import path
 
@@ -62,3 +63,9 @@ def on_categorized_annex_updated(annex, event):
                         file_data['filename'] = new_filename + ext
                     # file_uid is only there one time per session
                     break
+
+
+def on_annex_will_be_removed(annex, event):
+    '''Called when an annex will be removed, before the removed event.'''
+    # here annex is already unindexed
+    remove_files_from_session([annex.UID()])

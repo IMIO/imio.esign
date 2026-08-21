7bda96d163bfc16a575ae9c744f8cdd356290baa
Gauthier Bastien <gauthier.bastien@imio.be>
2026-08-21 16:41:54 +0200

    Added warning

--- src/imio/esign/adapters.py
+++ src/imio/esign/adapters.py
@@ -114,5 +114,7 @@ class ISignable(Interface):
     def get_create_session_custom_data(self):
         """
         Dict of custom data that will be stored on session at creation time.
+        This must be managed carefully to not use and overwrite data already existing
+        in the annotation.
         """
         return {}
